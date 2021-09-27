ff02c6c9da5a7dc1770408877cef13ea2a409d67
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2021-09-27 17:08:24 -0400

    Update 2

--- proxy/README.md
+++ proxy/README.md
@@ -1,4 +1,4 @@
-# Prox
+# Proxy
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.7.
 
--- proxy/src/app/app.component.spec.ts
+++ proxy/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prox'`, () => {
+  it(`should have as title 'proxy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prox');
+    expect(app.title).toEqual('proxy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prox app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proxy app is running!');
   });
 });
