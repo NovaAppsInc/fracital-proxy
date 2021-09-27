b360f65aad13287ed31f1be4af13ecee15829aa5
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2021-09-27 17:54:57 -0400

    Update 3

--- proxy/src/app/app.module.ts
+++ proxy/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import {MatSnackBar} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MatSnackBar
   ],
   providers: [],
   bootstrap: [AppComponent]
