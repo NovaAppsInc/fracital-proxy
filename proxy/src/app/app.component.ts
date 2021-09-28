2389a9a31decdde4abbebea2a8be15c7f3882992
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2021-09-28 17:10:42 -0400

    Update 4

--- proxy/src/app/app.component.ts
+++ proxy/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var name: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'proxy';
+  options: any;
+  api: any;
 }
---
+++ proxy/src/js/hover.js
@@ -0,0 +1,6 @@
+let searchbtn = document.getElementById("icon-search");
+let btnsrch = document.getElementById("btnsrch");
+
+btnsrch.onmouseover = e => {
+  searchbtn.style.stroke = "red";
+}
