e6b890b0b922ef6acb1227950f914ece23f79d99
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2021-10-09 18:32:15 -0700

    up

--- proxy/main/js/home.js
+++ proxy/main/js/home.js
@@ -1,7 +1,19 @@
+const frame = document.getElementById("frame"); 
+
 function clig1() {
     window.open("./g.html", "_self");
 }
 
 function bug() {
-    window.open("https://github.com/NovaAppsInc/fractrial-proxy/issues", "_blank");
+    window.open("https://github.com/NovaAppsInc/fractrial-proxy/issues", "_self");
+}
+
+function reloadh() {
+    frame.setAttribute('src', 'home.html');
+}
+
+const isMobile = navigator.userAgentData.mobile;
+
+if(isMobile) {
+    frame.setAttribute("src", "what.html")
 }
