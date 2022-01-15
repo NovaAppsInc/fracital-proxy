91cccc221deaac2d39a7ed6b735d4dbce60aca19
Linux Snoot <49733954+NovaAppsInc@users.noreply.github.com>
2022-01-15 15:27:15 -0500

    ui and mobile support updates
    new ui with new mobile support

--- proxy/main/js/agent.js
+++ proxy/main/js/agent.js
@@ -1,5 +1,24 @@
+const searchbar = document.getElementById("urlbar");
+const search = document.getElementById("search");
+const searchwrap = document.getElementById("searchwrap");
+const btnsrch = document.getElementById("btnsrch");
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    window.open("./what.html", "_self");
+
 }else{
     console.log("yep");
 }
+
+searchbar.addEventListener("click", () => {
+    if(searchwrap.classList.contains("minbar")) {
+        search.classList.remove("searchwrap");
+        search.classList.add("expand");
+        btnsrch.classList.add("expandbel");
+        searchwrap.classList.add("expandbel");
+    }
+
+    // search.focus();
+    // if(search.matches(":focus")) {
+    //     console.log("FUCK YEAH");
+    // }
+});
