f5a05b3a81b434dd0f9b51767805c9931b535b2b
Linux Snoot <49733954+NovaAppsInc@users.noreply.github.com>
2022-01-16 13:14:33 -0500

    updated ui
    - Updated UI
    - Added Fracital community link
    - fixed a UI bug

--- proxy/main/js/home.js
+++ proxy/main/js/home.js
@@ -1,3 +1,26 @@
+class xor {
+    static encode(str) {
+        return encodeURIComponent(str.toString().split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));
+    };
+    static decode(str) {
+        return decodeURIComponent(str.slice(0, -1)).split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join('');
+    };
+};
+
+function go(link) {
+  console.log(link);
+  if (link == '') {
+    alert('Bruh you need to insert a url!');
+  } else if (!link.includes("http://")) {
+        link = "http://" + link;
+    };
+    window.open("/kb/"+xor.encode(link), "_self");
+};
+
 function clig1() {
     window.open("./g.html", "_self");
+}
+
+function clig2() {
+    go("http://discord.gg/WzNvquznfw");
 }
