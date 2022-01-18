1a7f0d32aa5d2da9c27e4eb517a913f2a24f00db
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2022-01-18 00:21:09 -0500

    t

--- proxy/main/js/agent.js
+++ proxy/main/js/agent.js
@@ -2,23 +2,33 @@ const searchbar = document.getElementById("urlbar");
 const search = document.getElementById("search");
 const searchwrap = document.getElementById("searchwrap");
 const btnsrch = document.getElementById("btnsrch");
-
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-
-}else{
-    console.log("yep");
+function disableScroll() {
+    // Get the current page scroll positio
 }
 
-searchbar.addEventListener("click", () => {
-    if(searchwrap.classList.contains("minbar")) {
-        search.classList.remove("searchwrap");
-        search.classList.add("expand");
-        btnsrch.classList.add("expandbel");
-        searchwrap.classList.add("expandbel");
-    }
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    var wheelOpt = supportsPassive ? { passive: false } : false;
+    var supportsPassive = false;
+    try {
+      window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; } 
+      }));
+    } catch(e) {}
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+}else{
+    console.log("yepaa");
+}
 
-    // search.focus();
-    // if(search.matches(":focus")) {
-    //     console.log("FUCK YEAH");
-    // }
-});
+// searchbar.addEventListener("click", () => {
+//     if(searchwrap.classList.contains("minbar")) {
+//         search.classList.remove("searchwrap");
+//         search.classList.add("expand");
+//         btnsrch.classList.add("expandbel");
+//         searchwrap.classList.add("expandbel");
+//     }
+
+//     // search.focus();
+//     // if(search.matches(":focus")) {
+//     //     console.log("FUCK YEAH");
+//     // }
+// });
