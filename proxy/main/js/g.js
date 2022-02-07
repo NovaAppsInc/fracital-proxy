a8343bc9ba53db47ec83ab3e11c151ee7aead963
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2022-02-07 05:22:51 -0500

    added a game to games papge

--- proxy/main/js/g.js
+++ proxy/main/js/g.js
@@ -42,9 +42,18 @@ const hextrist = document.getElementById("hextrist");
 const towert = document.getElementById("towert");
 const simont = document.getElementById("simont");
 const jsracert = document.getElementById("jsracert");
+const retrot = document.getElementById("retrot");
+
+g2048.addEventListener("click", () => {
+    window.open("/gainly/2048/index.html", "_self");
+});
 
 g2048t.addEventListener("click", () => {
     window.open("/gainly/2048/index.html", "_self");
+});
+
+clumsy.addEventListener("click", () => {
+    window.open("/gainly/clumsy-bird/index.html", "_self")
 });
 
 clumsyt.addEventListener("click", () => {
@@ -56,18 +65,42 @@ clumsyt.addEventListener("click", () => {
 //   frame.focus();
 // });
 
+hextris.addEventListener("click", () => {
+    window.open("/gainly/hextris/index.html", "_self")
+});
+
 hextrist.addEventListener("click", () => {
     window.open("/gainly/hextris/index.html", "_self")
+});
+
+tower.addEventListener("click", () => {
+    window.open("/gainly/tower-building/index.html", "_self")
 });
 
 towert.addEventListener("click", () => {
     window.open("/gainly/tower-building/index.html", "_self")
 });
 
+simon.addEventListener("click", () => {
+    window.open("/gainly/simon-says/index.html", "_self")
+});
+
 simont.addEventListener("click", () => {
     window.open("/gainly/simon-says/index.html", "_self")
 });
 
+jsracer.addEventListener("click", () => {
+    window.open("/gainly/javascript-racer/v4.final.html", "_self")
+});
+
 jsracert.addEventListener("click", () => {
     window.open("/gainly/javascript-racer/v4.final.html", "_self")
+});
+
+retro.addEventListener("click", () => {
+    window.open("/gainly/retro-bowl/index.html", "_self")
+});
+
+retrot.addEventListener("click", () => {
+    window.open("/gainly/retro-bowl/index.html", "_self")
 });
